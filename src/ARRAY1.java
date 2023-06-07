41a609b4b6e6913025b5f057e27e1642ae6dc566
sonu choure <131000134+choure123@users.noreply.github.com>
2023-06-08 00:02:00 +0530

    ADDED:SOME PROBLEM

---
+++ src/ARRAY1.java
@@ -0,0 +1,25 @@
+public class ARRAY1 {
+    public static void main(String[] args) {
+        //declare and initialize the array
+        int[] arr = {50, 60, 55, 67, 70};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+class star {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
+
+
